07add465bf323af91e2001838a4aa45d74b2e067
katayama623 <k6t2y3m@gmail.com>
2020-08-07 11:59:11 +0000

    [confirm]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+	    @user = User.find(params[:id])
+	end
+
+	private
+	def user_params
+	    params.require(:user).permit(:name)
+	end
+
 end
