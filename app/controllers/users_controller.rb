2d22559d50c63333984625ef1e2c94c06e5522ff
katayama623 <k6t2y3m@gmail.com>
2020-08-12 11:57:53 +0000

    [add] グラフ表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@statuses = @user.statuses.all
+        @weights = @statuses.map(&:weight)
+        @dates = @statuses.map{|status| status.created_at.strftime('%Y/%m/%d') }
 	end
 
 	def edit
