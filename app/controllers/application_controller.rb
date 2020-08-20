80dbd35a127be2ff50f2ddb4412249ffad5b6d1d
katayama623 <k6t2y3m@gmail.com>
2020-08-20 12:25:21 +0000

    [add] 管理者画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
 
-	def after_sign_in_path_for(resource)
-        user_path(resource)
-    end
+
+    def after_sign_in_path_for(resource)
+		if user_signed_in?
+		    user_path(resource)
+		else
+		    admin_root_path
+		end
+	end
+
 
     def after_sign_out_path_for(resource)
         root_path
